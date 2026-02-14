561f9936bea322eb0a50d02ea3ccd3f25c09a014
Muhammed <lawalmuhammedope@gmail.com>
2026-02-14 14:10:38 +0100

    refactor: remove unnecessary RegisterPage function definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing/LandingPage";
 import LoginPage from "./pages/auth/LoginPage";
-
-function RegisterPage() {
-  return <div className="p-6">Register Page</div>;
-}
+import RegisterPage from "./pages/auth/RegisterPage";
 
 export default function App() {
   return (
